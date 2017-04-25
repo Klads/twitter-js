2fb2b9b09391e6632502e60bd88542086d2ca180
Kladky <sara.kladky@gmail.com>
2017-04-25 12:02:11 -0400

    static routing

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 
 const express = require( 'express' );
 const nunjucks = require( 'nunjucks' );
+const routes = require( './routes' );
 
 const app = express(); // creates an instance of an express application
 
 app.use('*', function (req, res, next) {
   console.log(req.method, req.originalUrl);
   next();
-})
+});
+
+app.use(express.static('public'));
+
+app.use('/', routes);
 
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
 app.set('view engine', 'html'); // have res.render work with html files
@@ -19,9 +24,14 @@ nunjucks.render('index.html', {title: 'Hall of Fame', people: people}, function 
     console.log(output);
 });
 
-app.get('/views', function(req, res, next) {
-	res.render('index.html', {title: 'Hall of Fame', people: people});
-})
+// app.get('/views', function(req, res, next) {
+// 	res.render('index.html', {title: 'Hall of Fame', people: people});
+// })
+
+// app.get('/stylesheets/style.css', function(req, res, next) {
+// 	res.sendFile(__dirname + '/public/stylesheets/style.css')
+// 	//console.log(__dirname);
+// })
 
 var server = app.listen(3000, () => {
 	console.log("Listening on port 3000")
